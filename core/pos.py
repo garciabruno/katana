2f2b1bc21336d4751e52ed2d0c7b2f59ad5f7712
nin <brunogarciaberrotaran@gmail.com>
2020-04-09 11:15:22 -0300

    Use a single prop to calculate timestamp delta. Added a method to return last active position for internal buses

--- core/pos.py
+++ core/pos.py
@@ -1,6 +1,7 @@
 import time
 from models import BusPos
 
+
 class Pos(object):
     def __init__(self, session, bus_line_id, time_delta=35):
         self.session = session
@@ -8,17 +9,30 @@ class Pos(object):
         self.time_delta = time_delta
 
     @property
-    def last_positions(self):
-        ts_delta = int(time.time()) - self.time_delta
+    def ts_delta(self):
+        return int(time.time()) - self.time_delta
 
-        last_positions = self.session.query(BusPos).filter(
-            BusPos.timestamp > ts_delta,
+    @property
+    def last_positions(self):
+        last_pos_all = self.session.query(BusPos).filter(
+            BusPos.timestamp > self.ts_delta,
             BusPos.bus_line_id == self.bus_line_id
         ).all()
 
-        return last_positions
-
+        return [x.as_dict() for x in last_pos_all]
 
     @property
     def last_active_internals(self):
-        return set([x.bus_internal_id for x in self.last_positions])
+        return set([x['bus_internal_id'] for x in self.last_positions])
+
+    def get_last_internal_positions(self, bus_internal_id):
+        # TODO: Make a geofence around every route's first position
+        # and ignore data from inside the fence
+
+        last_pos_internals = self.session.query(BusPos).filter(
+            BusPos.timestamp > self.ts_delta,
+            BusPos.bus_line_id == self.bus_line_id,
+            BusPos.bus_internal_id == bus_internal_id
+        ).all()
+
+        return [x.as_dict() for x in last_pos_internals]
