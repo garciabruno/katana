e1a8a5cbd21b476bbba3ca7812f1efaa973194c0
nin <brunogarciaberrotaran@gmail.com>
2020-04-09 11:17:50 -0300

    Fixed several tables not having correctly named columns. Added method to return objects as dictionaries in some models

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class BusLine(Base):
     __tablename__ = "bus_lines"
     id = Column(Integer, primary_key=True)
     line_id = Column(Integer)
-    email = Column(String)
+    line_description = Column(String)
 
 
 class BusRoute(Base):
@@ -21,13 +21,19 @@ class BusRoute(Base):
     route_id = Column(Integer)
     route_description = Column(String)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 
 class BusRoutePos(Base):
     __tablename__ = "bus_route_pos"
     id = Column(Integer, primary_key=True)
-    guild_id = Column(Integer, ForeignKey("bus_routes.route_id"), nullable=False)
+    route_id = Column(Integer, ForeignKey("bus_routes.route_id"), nullable=False)
     lat = Column(String)
     lon = Column(String)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class BusPos(Base):
@@ -40,20 +46,29 @@ class BusPos(Base):
     orientation = Column(Integer)
     timestamp = Column(Integer)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 
 class BusStop(Base):
     __tablename__ = "bus_stops"
     id = Column(Integer, primary_key=True)
-    route_id = Column(Integer, ForeignKey("bus_route.route_id"), nullable=False)
+    route_id = Column(Integer, ForeignKey("bus_routes.route_id"), nullable=False)
     lat = Column(String)
     lon = Column(String)
     stop_code = Column(String)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class BusTrip(Base):
     __tablename__ = "bus_trip"
     id = Column(Integer, primary_key=True)
-    bus_line_id = Column(Integer, ForeignKey("bus_lines.line_id"), nullable=False)
+
+    bus_line_id = Column(Integer)
     bus_internal_id = Column(Integer)
-    route_id = Column(Integer, ForeignKey("bus_route.route_id"), nullable=False)
+
+    route_id = Column(Integer)
     last_updated = Column(DateTime, default=datetime.utcnow)
+    last_pos_timestamp = Column(Integer, default=0)
